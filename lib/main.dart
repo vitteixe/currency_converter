be98a305920b3b13bf8184c393d23aff9504a9e0
Vitor Teixeira <vitoroliv@outlook.com>
2021-11-25 11:03:24 -0300

    Adicionando AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,49 @@
 import 'package:flutter/material.dart';
 
 import 'package:http/http.dart' as http;
-import 'package:async/async.dart';
+import 'dart:async';
 import 'dart:convert';
 
 const request = "https://api.hgbrasil.com/finance?format=json-cors&key=4c1a8bee";
 
 void main() async {
-
-  http.Response response = await http.get(Uri.parse(request));
-  print(json.decode(response.body)["results"]["currencies"]["USD"]);
-
-
   runApp(MaterialApp(
-    home: Container ()
+    debugShowCheckedModeBanner: false,
+    home: Home()
   ));
+}
+
+Future<Map> getData() async{
+  http.Response response = await http.get(Uri.parse(request));
+  return json.decode(response.body);
+}
+
+
+//--                STFUL                   --//
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+//--                                       --//
+
+class _HomeState extends State<Home> {
+
+  @override
+  Widget build(BuildContext context) {
+    return
+      Scaffold(
+      backgroundColor: Colors.black38,
+      appBar: AppBar(
+        title: Text("Conversor"),
+        backgroundColor: Colors.amber,
+        centerTitle: true,
+      ),
+
+      
+    );
+  }
 }
